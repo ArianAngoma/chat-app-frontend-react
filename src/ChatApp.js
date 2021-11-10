90b58a82b098080063e3f97d0480f4ec6fd94147
unknown <arian.angoma@tecsup.edu.pe>
2021-11-10 08:13:32 -0500

    AuthProvider y AuthContext creados

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,10 +1,11 @@
 /* Importaciones propias */
 import {AppRouter} from './router/AppRouter';
+import {AuthProvider} from './auth/AuthContext';
 
 export const ChatApp = () => {
     return (
-        <div>
+        <AuthProvider>
             <AppRouter/>
-        </div>
+        </AuthProvider>
     )
 }
