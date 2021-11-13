fe9cae5847ebb9cb5fe539c2b8444f3a57720e12
unknown <arian.angoma@tecsup.edu.pe>
2021-11-13 11:12:35 -0500

    Formulario de enviar un nuevo mensaje

--- src/components/chat/SendMessage.js
+++ src/components/chat/SendMessage.js
@@ -1,9 +1,30 @@
+import {useState} from 'react';
+
 export const SendMessage = () => {
+    const [message, setMessage] = useState('');
+
+    const handleChange = ({target}) => {
+        setMessage(target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        /* Validar si el campo de mensaje está vacio */
+        if (!message.length) return null;
+
+        /* Limpiar formulario */
+        setMessage('');
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="type_msg row">
                 <div className="input_msg_write col-sm-9">
-                    <input type="text" className="write_msg" placeholder="Mensaje..."/>
+                    <input type="text" className="write_msg" placeholder="Mensaje..."
+                           name={message}
+                           value={message}
+                           onChange={handleChange}/>
                 </div>
 
                 <div className="col-sm-3 text-center">
