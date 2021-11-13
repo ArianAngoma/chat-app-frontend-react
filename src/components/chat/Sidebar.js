7412938401077edc4612bad7f7640483f90da99c
unknown <arian.angoma@tecsup.edu.pe>
2021-11-13 07:40:37 -0500

    Mostrar todos los usuarios en el Sidebar del Chat

--- src/components/chat/Sidebar.js
+++ src/components/chat/Sidebar.js
@@ -1,15 +1,28 @@
+import {useContext} from 'react';
+
 /* Importaciones propias */
 import {SidebarChatItem} from './SidebarChatItem';
+import {ChatContext} from '../../context/chat/ChatContext';
+import {AuthContext} from '../../auth/AuthContext';
 
 export const Sidebar = () => {
-    const chats = [1, 2, 3, 4, 5];
+    const {chatState} = useContext(ChatContext);
+
+    /* Obtener los datos del usuario logueado */
+    const {auth} = useContext(AuthContext);
+
+    const {uid} = auth;
 
     return (
         <div className="inbox_chat">
             {
-                chats.map(chat => (
-                    <SidebarChatItem key={chat}/>
-                ))
+                chatState.users
+                    /* Filtrar todos los usuario, excepto el logueado */
+                    .filter(user => user.uid !== uid)
+                    .map(user => (
+                        <SidebarChatItem key={user.uid}
+                                         user={user}/>
+                    ))
             }
 
             {/* Espacio extra para el scroll */}
