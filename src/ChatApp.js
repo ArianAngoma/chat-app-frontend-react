15acc63258ab2045b02984cdad2c8b1f0f51a663
unknown <arian.angoma@tecsup.edu.pe>
2021-11-13 13:34:46 -0500

    Mostrar fecha y hora del mensaje

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,8 +1,14 @@
+import moment from 'moment';
+import 'moment/locale/es';
+
 /* Importaciones propias */
 import {AppRouter} from './router/AppRouter';
 import {AuthProvider} from './auth/AuthContext';
 import {SocketProvider} from './context/SocketContext';
 import {ChatProvider} from './context/chat/ChatContext';
+
+/* Configuración del idioma de moment */
+moment.locale('es');
 
 export const ChatApp = () => {
     return (
